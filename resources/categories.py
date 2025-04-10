f3434bce9b52668f797f083b979cda7b91fd2746
oscar <oscar.avp.mamani@gmail.com>
2025-04-10 17:14:52 -0500

    Añadir README

---
+++ resources/categories.py
@@ -0,0 +1,32 @@
+
+CATEGORIES = {
+    "animales": [
+        "perro", "gato", "elefante", "jirafa", "leon", "tigre", "cebra", 
+        "delfin", "ballena", "mono", "aguila", "serpiente", "cocodrilo", 
+        "tortuga", "pinguino", "koala", "canguro", "panda", "rinoceronte"
+    ],
+    
+    "paises": [
+        "españa", "mexico", "argentina", "colombia", "peru", "brasil", 
+        "francia", "italia", "china", "japon", "canada", "australia", 
+        "alemania", "rusia", "portugal", "egipto", "marruecos", "sudafrica"
+    ],
+    
+    "frutas": [
+        "manzana", "platano", "naranja", "fresa", "uva", "sandia", "melon", 
+        "piña", "mango", "durazno", "cereza", "pera", "kiwi", "limon", 
+        "mandarina", "coco", "arandano", "papaya", "ciruela"
+    ],
+    
+    "deportes": [
+        "futbol", "baloncesto", "tenis", "natacion", "atletismo", "golf", 
+        "boxeo", "ciclismo", "voleibol", "hockey", "rugby", "beisbol", 
+        "karate", "esqui", "surf", "patinaje", "judo", "ajedrez", "gimnasia"
+    ],
+    
+    # Añade esta nueva categoría al diccionario CATEGORIAS
+"profesiones": [
+    "medico", "profesor", "ingeniero", "abogado", "arquitecto", "bombero",
+    "policia", "piloto", "chef", "electricista", "carpintero", "enfermero"
+]
+}
